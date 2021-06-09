e9b5ed610dfdcce60ca0e85253cd57e8c6f77a8b
Manav Singh <54039581+maxmanav@users.noreply.github.com>
2021-06-10 01:38:58 +0530

    Add files via upload

---
+++ Hirst_painting.py
@@ -0,0 +1,46 @@
+#import colorgram
+#
+#rgb_colors = []
+#colors = colorgram.extract('image.jpg', 30)
+#for color in colors:
+#    r= color.rgb.r
+#    g = color.rgb.g
+#    b =color.rgb.b
+#    new_colors = (r,g,b)
+#    rgb_colors.append(new_colors)
+#
+#print(rgb_colors)
+import turtle as t
+import random
+
+t.colormode(255)
+
+color_list = [(245, 243, 238), (247, 242, 244), (240, 245, 241), (202, 164, 109), (238, 240, 245), (150, 75, 49), (223, 201, 135), (52, 93, 124), (172, 154, 40), (140, 30, 19), (133, 163, 185), (198, 91, 71), (46, 122, 86), (72, 43, 35), (145, 178, 148), (13, 99, 71), (233, 175, 164), (161, 142, 158), (105, 74, 77), (55, 46, 50), (183, 205, 171), (36, 60, 74), (18, 86, 90), (81, 148, 129), (148, 17, 20), (14, 70, 64), (30, 68, 100), (107, 127, 153), (174, 94, 97), (176, 192, 209)]
+[(245, 243, 238), (247, 242, 244), (240, 245, 241), (202, 164, 109), (238, 240, 245), (150, 75, 49), (223, 201, 135), (52, 93, 124), (172, 154, 40), (140, 30, 19), (133, 163, 185), (198, 91, 71), (46, 122, 86), (72, 43, 35), (145, 178, 148), (13, 99, 71), (233, 175, 164), (161, 142, 158), (105, 74, 77), (55, 46, 50), (183, 205, 171), (36, 60, 74), (18, 86, 90), (81, 148, 129), (148, 17, 20), (14, 70, 64), (30, 68, 100), (107, 127, 153), (174, 94, 97), (176, 192, 209)]
+
+timmy = t.Turtle()
+timmy.hideturtle()
+timmy.penup()
+timmy.speed("fastest")
+timmy.setheading(225)
+timmy.forward(300)
+timmy.setheading(0)
+no_dots= 100
+
+for i in range(1,no_dots+1):
+    timmy.dot(20,random.choice(color_list))
+    timmy.forward(50)
+    
+    
+    if i % 10==0:
+        timmy.setheading(90)
+        timmy.forward(50)
+        timmy.setheading(180)
+        timmy.forward(500)
+        timmy.setheading(0)
+        
+
+
+
+screen = t.Screen()
+screen.exitonclick()
